00f45ff18002f03d41920842d3443a03a885a495
Collinmc1228 <collinmc1228@gmail.com>
2021-11-27 22:14:44 -0500

    main.kys lol

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-    import * as Minecraft from "mojang-minecraft";
-    import { nochatspam } from "./index";
+import * as Minecraft from "mojang-minecraft";
+import { nochatspam } from "./index";
 
-    const World = Minecraft.World;
-    const Commands = Minecraft.Commands;
+const World = Minecraft.World;
+const Commands = Minecraft.Commands;
 
-    World.events.tick.subscribe(() => {
+World.events.tick.subscribe(() => {
         // run as each player
-        for (let player of World.getPlayers()) { 
+for (let player of World.getPlayers()) { 
 
             // player position shit
-            Commands.run(`scoreboard players set "${player.nameTag}" xCoord ${Math.floor(player.location.x)}`, World.getDimension("overworld"));
-            Commands.run(`scoreboard players set "${player.nameTag}" yCoord ${Math.floor(player.location.y)}`, World.getDimension("overworld"));
-            Commands.run(`scoreboard players set "${player.nameTag}" zCoord ${Math.floor(player.location.z)}`, World.getDimension("overworld"));
-    }})
+    Commands.run(`scoreboard players set "${player.nameTag}" xCoord ${Math.floor(player.location.x)}`, World.getDimension("overworld"));
+    Commands.run(`scoreboard players set "${player.nameTag}" yCoord ${Math.floor(player.location.y)}`, World.getDimension("overworld"));
+    Commands.run(`scoreboard players set "${player.nameTag}" zCoord ${Math.floor(player.location.z)}`, World.getDimension("overworld"));
+}})
