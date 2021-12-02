6b75d38de4a570198f667b27416626b0ce7f45e4
Collinmc1228 <collinmc1228@gmail.com>
2021-12-01 21:01:25 -0500

    js maybe

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
-import { World, Commands } from 'mojang-minecraft'
+import { World, Commands } from 'mojang-minecraft';
 import * as GameTest from "mojang-gametest"; 
+
+ World.events.tick.subscribe(()=>{
+  World:getPlayers().forEach(player=>{
 let playerPosX = player.location.x;
 let playerPosY = player.location.y;
 let playerPosZ = player.location.z;
     Commands.run(`scoreboard players set "${player.nameTag}" xCoord ${Math.floor(player.location.x)}`, world.getDimension("overworld"));
     Commands.run(`scoreboard players set "${player.nameTag}" yCoord ${Math.floor(player.location.y)}`, world.getDimension("overworld"));
     Commands.run(`scoreboard players set "${player.nameTag}" zCoord ${Math.floor(player.location.z)}`, world.getDimension("overworld"));
+  })
+})
