7740b7d378afac7ce86fb033d632c807614f683f
Collinmc1228 <collinmc1228@gmail.com>
2021-12-01 20:50:25 -0500

    idk tbh

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ import * as GameTest from "mojang-gametest";
 let playerPosX = player.location.x
 let playerPosY = player.location.y;
 let playerPosZ = player.location.z;
-    Commands.run(`scoreboard players set "{$player.nameTag}" xCoord ${player.location.x}`, world.getDimension("overworld"));
-    Commands.run(`scoreboard players set "{$player.nameTag}" yCoord ${player.location.y}`, world.getDimension("overworld"));
-    Commands.run(`scoreboard players set "{$player.nameTag}" zCoord ${player.location.z}`, world.getDimension("overworld"));
+Commands.run(`scoreboard players set "{$player.nameTag}" xCoord ${player.location.x}`, world.getDimension("overworld"));
+Commands.run(`scoreboard players set "{$player.nameTag}" yCoord ${player.location.y}`, world.getDimension("overworld"));
+Commands.run(`scoreboard players set "{$player.nameTag}" zCoord ${player.location.z}`, world.getDimension("overworld"));
