44d9ecb5ce6ea6bc77db59ae7829a0c3ec7322cd
Collinmc1228 <collinmc1228@gmail.com>
2021-12-01 20:57:26 -0500

    sussy js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 import { World, Commands } from 'mojang-minecraft'
 import * as GameTest from "mojang-gametest"; 
-let playerPosX = player.location.x
+let playerPosX = player.location.x;
 let playerPosY = player.location.y;
 let playerPosZ = player.location.z;
-Commands.run(`scoreboard players set "{$player.nameTag}" xCoord ${player.location.x}`, world.getDimension("overworld"));
-Commands.run(`scoreboard players set "{$player.nameTag}" yCoord ${player.location.y}`, world.getDimension("overworld"));
-Commands.run(`scoreboard players set "{$player.nameTag}" zCoord ${player.location.z}`, world.getDimension("overworld"));
+    Commands.run(`scoreboard players set "${player.nameTag}" xCoord ${Math.floor(player.location.x)}`, world.getDimension("overworld"));
+    Commands.run(`scoreboard players set "${player.nameTag}" yCoord ${Math.floor(player.location.y)}`, world.getDimension("overworld"));
+    Commands.run(`scoreboard players set "${player.nameTag}" zCoord ${Math.floor(player.location.z)}`, world.getDimension("overworld"));
